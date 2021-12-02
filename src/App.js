684b416b14d1389110ee04e5035aa0c5dc6871ab
Nicholas <nikhsd899@outlook.com>
2021-12-02 14:52:44 -0800

    commit tree e80da8d10e7ad32e502b40539fb13a5b73da3277

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 import BillCollection from './components/BillCollection';
 import BillsCast from './components/BillsCast';
 
+const API = "http://localhost:8002/bills";
+
 export default function App() {
+  const [bills, setBills] = useState([]);
+
   //start here with your code for step one
+  useEffect(() => {
+    fetch(API) 
+    .then((res) => res.json())
+    .then((json) => setBills(json));
+  }, []);
+
+  function castBill(bill) {
+    setBills(bills.map(b => b.id === bill.id ? {...b, cast: true} : b));
+  }
+
+  function fireBill(bill) {
+    setBills(bills.map(b => b.id === bill.id ? {...b, cast: false} : b ));
+  }
 
   return (
     <div>
-      <BillsCast />
-      <BillCollection />
+      <BillsCast castedBills={bills.filter(b => b.cast)} firebill={fireBill} />
+      <BillCollection bills={bills} castBill={castBill} />
     </div>
   );
 }
--- src/components/BillsCast.js
+++ src/components/BillsCast.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import BillCard from './BillCard';
 
-export default function BillsCast() {
-  // Your code here
-
+export default function BillsCast({castedBills, fireBill}) {
   return (
     <div className="ui segment inverted blue bill-cast">
       <div className="ui five column grid">
         <div className="row bill-cast-row">
-        {/*...and here..*/}
+          {/*...and here..*/}
         </div>
       </div>
     </div>
-  );
+  )
 }
+
+  
 
